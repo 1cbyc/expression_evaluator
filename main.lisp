bd9845ef07ba7a9f746466bd3e18575aab127c06
1cbyc <isaacnsisong@gmail.com>
2024-12-02 13:04:30 +0100

    trying to write something public with lisp

---
+++ main.lisp
@@ -0,0 +1,14 @@
+(defun evaluate-expression (expr)
+  "Evaluates a mathematical expression represented as a Lisp list."
+  (cond
+    ((numberp expr) expr)  ; Base case: if it's a number, return it.
+    ((listp expr)          ; If it's a list, process recursively.
+     (let ((op (car expr))
+           (args (cdr expr)))
+       (case op
+         (+ (reduce #'+ (mapcar #'evaluate-expression args)))
+         (- (reduce #'- (mapcar #'evaluate-expression args)))
+         (* (reduce #'* (mapcar #'evaluate-expression args)))
+         (/ (reduce #'/ (mapcar #'evaluate-expression args)))
+         (t (error "Unknown operator: ~A" op)))))
+    (t (error "Invalid expression: ~A" expr))))
